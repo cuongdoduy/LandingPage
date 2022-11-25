3cb791826f8c832366adf0e8a5767d08f16c4afa
CuongDoDuy <cuongthtb08022004@gmail.com>
2022-11-25 10:22:18 +0700

    finally

--- js/app.js
+++ js/app.js
@@ -1,68 +1,64 @@
 // build the nav
-var sections=document.querySelectorAll("section");
-var text=[];
-var Idarray=[];
-for(var section of sections)
-{
+const sections = document.querySelectorAll("section");
+const text = [];
+const Idarray = [];
+for (const section of sections) {
     text.push(section.querySelector('h2').innerText);
     Idarray.push(section.id);
 }
 
-var navbar=document.getElementById('navbar__list');
-let i=0;
-for(var TextElement of text)
-{
-    var node=document.createElement("li");
-    var node2=document.createElement("a")
-    node2.href=`#${Idarray[i]}`;
+const navbar = document.getElementById('navbar__list');
+let i = 0;
+for (const TextElement of text) {
+    const node = document.createElement("li");
+    const node2 = document.createElement("a")
+    node2.href = `#${Idarray[i]}`;
     node.appendChild(node2);
-    node2.textContent=TextElement;
+    node2.textContent = TextElement;
     node2.classList.add("menu__link");
     navbar.appendChild(node);
-    i+=1;
+    i += 1;
 }
-var ElementNavbar=navbar.querySelectorAll('a');
+const ElementNavbar = navbar.querySelectorAll('a');
 // Add class 'active' to section when near top of viewport
-i=1;
+i = 1;
 
-    window.onscroll=function() {
-        for(element of ElementNavbar)
-        {
-            var result=element.href.substring(element.href.length,element.href.search("#"));
-            if (document.querySelector(result).getBoundingClientRect().top.toFixed()>=-20 && document.querySelector(result).getBoundingClientRect().top.toFixed()<=100 ) 
-            {
-                document.querySelector(result).classList.add("your-active-class");
-                // Add underline to navbar
-                for(nav of ElementNavbar )
-                {
-                    if (nav.href.substring(nav.href.length,nav.href.search("#"))==result)
-                    {
-                        nav.classList.add("texttransform");
-                    }
-                    else
-                    {
-                        nav.classList.remove("texttransform");
-                    }                  
-                    
+window.onscroll = function () {
+    for (element of ElementNavbar) {
+        const result = element.href.substring(element.href.length, element.href.search("#"));
+        const distance = document.querySelector(result).getBoundingClientRect().top.toFixed();
+        if (distance >= -20 && distance <= 100) {
+            document.querySelector(result).classList.add("your-active-class");
+            // Add underline to navbar
+            for (nav of ElementNavbar) {
+                if (nav.href.substring(nav.href.length, nav.href.search("#")) == result) {
+                    nav.classList.add("texttransform");
+                } else {
+                    nav.classList.remove("texttransform");
                 }
+
             }
-            else
-            document.querySelector(result).classList.remove("your-active-class")
         }
+        else
+            document.querySelector(result).classList.remove("your-active-class")
     }
+}
 
 
 
 // Scroll to anchor ID using scrollTO event
 
-for(element of ElementNavbar)
-{
-    element.addEventListener("click", function(event) { 
+for (element of ElementNavbar) {
+    element.addEventListener("click", function (event) {
         event.preventDefault(); // prevent default action l.e redirecting 
-        var result=event.target.href.substring(event.target.href.length,event.target.href.search("#"));
-        window.scrollTo(0,document.querySelector(result).offsetTop);
+        const result = event.target.href.substring(event.target.href.length, event.target.href.search("#"));
+        window.scrollTo({
+            behavior: "smooth",
+            top: document.querySelector(result).offsetTop,
+        });
+
     })
-     
+
 }
 
 
--- readme.md
+++ readme.md
@@ -1,9 +1,14 @@
-## Landing Page
+# Landing Page
+## Use JavaScript to improve the user experience
 ### DemoPreview
 * https://cuongdoduy.github.io/LandingPage/ * 
-### Table of content
-* Title
-* Section 1
-* Section 2
-* Section 3
-* Footer
+## Feature
+* The page automatically scrolls to the desired section when being clicked.
+* Auto add CSS class active state when the element is in the viewport.
+* Auto underline to the element in navbar when it is in the viewport.
+## Technology
+* Index.html and style.css are provided by Udacity.
+* App.js is included all features listed above.
+* Developed by [Cuong.DoDuy] 
+    [Cuong.DoDuy]: <https://github.com/cuongdoduy>
+
